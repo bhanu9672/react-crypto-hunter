86e124199badb0feb3b9838a52e1b6a5256dc7e5
BHANU PRATAP SINGH <bhanubhanu7737@gmail.com>
2022-10-31 22:54:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Profile from "./Components/Profile";
 function App() {
 
   const [coins, setCoins] = useState( [] )
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false'
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
 
   useEffect(() => {
     axios.get(url).then((response) => {
