ed3f0384fd26e52c8fbaaef89b97443803e7e179
BHANU PRATAP SINGH <bhanubhanu7737@gmail.com>
2022-10-31 22:51:22 +0530

    Add Carousel & Search Crypto Coin

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,24 +1,84 @@
-import React from 'react'
-import AliceCarousel from 'react-alice-carousel';
+import React, { Component } from 'react';
+// And react-slick as our Carousel Lib
+import Slider from 'react-slick';
+import { Image } from 'react-bootstrap';
+import axios from "axios";
+import { Heading, Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom';
-import Carousel from 'react-elastic-carousel'
 
-const CarouselC = () => {
+const Carousel = () => {
 
+    const [trandings, SetTrandings] = React.useState([])
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false'
+
+    React.useEffect(() => {
+        axios.get(url).then((response) => {
+            SetTrandings(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
+    console.table(trandings);
+    console.log(trandings);
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500
+    };
 
     return (
         <>
-            <Carousel enableAutoPlay autoPlaySpeed={1500}>
-                11
-                {/* <Item>1</Item>
-                <Item>2</Item>
-                <Item>3</Item>
-                <Item>4</Item>
-                <Item>5</Item>
-                <Item>6</Item> */}
-            </Carousel>
+
+            <div className='text-center my-5'>
+                <Heading mb={4}>Crypto Hunter</Heading>
+                <Heading as='h5' size='md'>
+                    Get All The Info Regarding Your Favorite Crypto Currency
+                </Heading>
+            </div>
+
+            {
+                trandings.length > 0 ?
+                    <Slider {...settings}>
+                        {
+                            trandings.map((tranding) =>
+                                <>
+                                    <div key={tranding.id}>
+                                        <Link to={`/coin/${tranding.id}`}>Go Link
+                                            <h1>{tranding.name}</h1>
+                                            <h2>$ {tranding.current_price}</h2>
+                                            <h2>
+                                                {
+                                                    tranding.market_cap_change_percentage_24h >= 0 ?
+                                                        <>+{tranding.market_cap_change_percentage_24h}</> :
+                                                        tranding.market_cap_change_percentage_24h
+                                                }
+                                            </h2>
+                                            <h3>
+                                                <Image
+                                                    src={tranding.image}
+                                                />
+                                            </h3>
+                                        </Link>
+                                    </div>
+                                </>
+                            )
+                        }
+                    </Slider> :
+                    <>
+                        <Stack>
+                            <Skeleton height='20px' />
+                            <Skeleton height='20px' />
+                            <Skeleton height='20px' />
+                        </Stack>
+                    </>
+            }
         </>
     )
 }
 
-export default CarouselC
+export default Carousel
--- src/Components/Coins.jsx
+++ src/Components/Coins.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CoinItems from './CoinItems';
 import Coin from '../routes/Coin';
-
+import Carousel from './Carousel';
 import {
     Table,
     Thead,
@@ -14,11 +14,30 @@ import {
     TableContainer,
     Stack,
     Skeleton,
+    Heading,
+    Center,
 } from '@chakra-ui/react'
+import Form from 'react-bootstrap/Form';
 
 const Coins = (props) => {
+
+    const [search, Setsearch] = useState('');
+    console.warn(search);
+
     return (
         <>
+            <Carousel />
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Search Crypto Coin</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Search Crypto Coin"
+                        onChange={(e) => Setsearch(e.target.value)}
+                    />
+                </Form.Group>
+            </Form>
+            {search}
             {
                 props.coins == 0 ?
                     <>
@@ -33,9 +52,13 @@ const Coins = (props) => {
                     </>
                     :
                     <>
+                        <Center>
+                            <Heading size='lg' fontSize='30px'>
+                                Cryptocurrency Prices by Market Cap
+                            </Heading>
+                        </Center>
                         <TableContainer>
                             <Table variant='simple'>
-                                <TableCaption>Imperial to metric conversion factors</TableCaption>
                                 <Thead>
                                     <Tr>
                                         <Th>#</Th>
@@ -49,15 +72,23 @@ const Coins = (props) => {
                                 </Thead>
                                 <Tbody>
                                     {
-                                        props.coins.map((coins) => {
-                                            return (
-                                                <>
-                                                    {/* <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id} > */}
-                                                    <CoinItems coins={coins} key={coins.id} />
-                                                    {/* </Link> */}
-                                                </>
-                                            )
-                                        })
+                                        props.coins.filter(
+                                            coins => {
+                                                return (
+                                                    coins.name.toLowerCase().includes(search) ||
+                                                    coins.name.includes(search) ||
+                                                    coins.symbol.toLowerCase().includes(search) 
+                                                );
+                                            })
+                                            .map(coins => {
+                                                return (
+                                                    <>
+                                                        {/* <Link to={`/coin/${coins.id}`} element={<Coin />} key={coins.id} > */}
+                                                        <CoinItems coins={coins} key={coins.id} />
+                                                        {/* </Link> */}
+                                                    </>
+                                                )
+                                            })
                                     }
                                 </Tbody>
                                 <Tfoot>
